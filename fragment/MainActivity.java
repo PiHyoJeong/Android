c124bcf5209b451155f8361a22e124303f251d6a
PiHyoJeong <78262975+PiHyoJeong@users.noreply.github.com>
2021-06-17 17:05:39 +0900

    humanclass_17JUN21_fragment

---
+++ fragment/MainActivity.java
@@ -0,0 +1,33 @@
+package com.techtwon.fragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    MainFragment mainFragment;
+    MenuFragment menuFragment;
+    SubFragment subFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mainFragment = (MainFragment)getSupportFragmentManager().findFragmentById(R.id.mainFragment);
+        menuFragment = new MenuFragment();
+        subFragment = new SubFragment();
+
+    }
+
+    public void onFragmentChanged(int index){
+        if(index == 0){
+            getSupportFragmentManager().beginTransaction().replace(R.id.container,menuFragment).commit();
+        }else if(index ==1){
+            getSupportFragmentManager().beginTransaction().replace(R.id.container,mainFragment).commit();
+        }else if(index ==2){
+            getSupportFragmentManager().beginTransaction().replace(R.id.container,subFragment).commit();
+        }
+    }
+}
---
+++ fragment/SubFragment.java
@@ -0,0 +1,27 @@
+package com.techtwon.fragment;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+public class SubFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_sub, container, false);
+        Button button = rootView.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity activity = (MainActivity) getActivity();
+                activity.onFragmentChanged(0);
+            }
+        });
+        return rootView;
+    }
+
+}
